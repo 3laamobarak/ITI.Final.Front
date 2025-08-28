07744ba65554d04830f281b94b28cf898d7dc748
Alaa Mobarak <3laa.m0o0barak@gmail.com>
2025-08-28 20:23:16 +0300

    done the authentication guards locoliztion to go

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,18 +6,18 @@ import { ProductList } from './Components/products/product-list/product-list';
 import { ProductDetails } from './Components/products/product-details/product-details';
 import { LoginRegister } from './Components/login-register/login-register';
 import { VerifyEmail } from './Components/verify-email/verify-email';
+import {noAuthGuard} from './Guards/no-auth-guard';
 
 export const routes: Routes = [
 
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-    {path: 'home' , component: Home},
+    {path: 'home' , component: Home , title: 'Home'},
     {path: 'productsList' , component: ProductList},
     { path: 'product/:id', component: ProductDetails },
     {path: 'NotFound' , component: NotFound},
-    {path: 'login-register' , component: LoginRegister},
-    {path: 'verify-email' , component: VerifyEmail},
-    
+    {path: 'login-register' , component: LoginRegister , title: 'Login / Register' , canActivate: [noAuthGuard]},
+    {path: 'verify-email' , component: VerifyEmail , title: 'Verify Email' , canActivate: [noAuthGuard]},
 
     {path: '**', redirectTo: '/NotFound'}
-   
+
 ];
